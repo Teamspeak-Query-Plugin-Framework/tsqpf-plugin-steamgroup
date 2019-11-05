e8c37b16fc0ea883a30d8427e8bc164cd80aa1f9
TAXSET <sandro.kierner@gmail.com>
2019-11-05 15:34:49 +0100

    Added Main and plugin.yml

---
+++ src/net/vortexdata/tsqpf_plugin_steamgroup/Main.java
@@ -0,0 +1,38 @@
+package net.vortexdata.tsqpf_plugin_steamgroup;
+
+import net.vortexdata.tsqpf.plugins.TeamspeakPlugin;
+import net.vortexdata.tsqpf_plugin_steamgroup.commands.chat.CSteamGroup;
+import sun.awt.image.ImageWatched;
+
+public class Main extends TeamspeakPlugin {
+
+    private LinkManager linkManager;
+
+    @Override
+    public void onEnable() {
+
+        getConfig().setDefault("steamGroupUrl", "https://steamcommunity.com/groups/vortexdatanet");
+        getConfig().setDefault("linkGroupId", "1");
+        getConfig().setDefault("messageLinkCreated", "Congratulations! You have now been assigned the steam verified group.");
+        getConfig().setDefault("messageLinkClientNotFound", "Sorry, but I couldn't find this username.");
+        getConfig().setDefault("messageLinkClientNotVerified", "Sorry, but I couldn't verify that this account belongs to you.");
+        getConfig().setDefault("messageLinkRemoved", "Your account has been successfully unlinked.");
+        getConfig().setDefault("messagePinCreated", "Your verification pin has been created. Please paste it in to your steam profiles info box. Your pin is: ");
+        getConfig().setDefault("messageGuide", "First run '!steamgroup link <STEAM_PROFILE_URL>'. This will generate a PIN which you have to paste in to your Steam profiles info box. After you have done that, run '!steamgroup verify <YOUR_STEAM_URL>'. If you've followed all steps your account should now be linked.");
+        getConfig().setDefault("messageSyntax", "!steamgroup <guide | pin | link | unlink>");
+        getConfig().saveAll();
+
+        registerChatCommand(new CSteamGroup(getAPI(), getConfig()), "!steamgroup");
+
+        linkManager = new LinkManager(getConfig().readValue("steamGroupUrl"));
+
+    }
+
+    @Override
+    public void onDisable() {
+
+
+
+    }
+
+}
