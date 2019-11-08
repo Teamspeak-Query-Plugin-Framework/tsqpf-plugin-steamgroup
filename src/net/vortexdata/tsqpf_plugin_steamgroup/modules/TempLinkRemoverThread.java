4077b7d7557c40cccfd331bfb78b16d3a7e3cbe0
TAXSET <sandro.kierner@gmail.com>
2019-11-08 21:38:58 +0100

    Added missing loop in collector service

--- src/net/vortexdata/tsqpf_plugin_steamgroup/modules/TempLinkRemoverThread.java
+++ src/net/vortexdata/tsqpf_plugin_steamgroup/modules/TempLinkRemoverThread.java
@@ -29,48 +29,50 @@ public class TempLinkRemoverThread implements Runnable {
     @Override
     public void run() {
 
-        try {
-            Thread.sleep(sleep * 1000);
-        } catch (InterruptedException e) {
+        while (true) {
+            try {
+                Thread.sleep(sleep * 1000);
+            } catch (InterruptedException e) {
 
-        }
+            }
 
-        logger.printDebug("Collecting and removing old temporary links...");
+            logger.printDebug("Collecting and removing old temporary links...");
 
-        BufferedReader br = null;
-        BufferedWriter bw = null;
-        ArrayList<String> validLinks = new ArrayList<>();
+            BufferedReader br = null;
+            BufferedWriter bw = null;
+            ArrayList<String> validLinks = new ArrayList<>();
 
-        try {
-            br = new BufferedReader(new FileReader(path + "templinks.txt"));
-            bw = new BufferedWriter(new FileWriter(path + "templinks.txt", true));
+            try {
+                br = new BufferedReader(new FileReader(path + "templinks.txt"));
+                bw = new BufferedWriter(new FileWriter(path + "templinks.txt", true));
 
-            String currentLine = "default";
-            while (currentLine != null && !currentLine.isEmpty()) {
-                currentLine = br.readLine();
-                if (currentLine == null || currentLine.isEmpty())
-                    break;
+                String currentLine = "default";
+                while (currentLine != null && !currentLine.isEmpty()) {
+                    currentLine = br.readLine();
+                    if (currentLine == null || currentLine.isEmpty())
+                        break;
 
-                String[] link = currentLine.split(";");
-                if (Long.parseLong(link[2]) > System.currentTimeMillis() - sleep) {
-                    validLinks.add(currentLine);
+                    String[] link = currentLine.split(";");
+                    if (Long.parseLong(link[2]) > System.currentTimeMillis() - sleep) {
+                        validLinks.add(currentLine);
+                    }
                 }
+
+                BufferedWriter bwr = new BufferedWriter(new FileWriter(path + "templinks.txt"));
+                bwr.write("");
+                for (String line : validLinks) {
+                    bw.write(line);
+                }
+
+            } catch (FileNotFoundException e) {
+                logger.printWarn("Temporary link collection file not found, please check your file write permissions.");
+            } catch (IOException e) {
+                logger.printWarn("Failed to write / read link collection file, please check your file write permissions.");
             }
 
-            BufferedWriter bwr = new BufferedWriter(new FileWriter(path + "templinks.txt"));
-            bwr.write("");
-            for (String line : validLinks) {
-                bw.write(line);
-            }
+            logger.printDebug("Temporary link collection finished.");
 
-        } catch (FileNotFoundException e) {
-            logger.printWarn("Temporary link collection file not found, please check your file write permissions.");
-        } catch (IOException e) {
-            logger.printWarn("Failed to write / read link collection file, please check your file write permissions.");
         }
-
-        logger.printDebug("Temporary link collection finished.");
-
     }
 
 }
